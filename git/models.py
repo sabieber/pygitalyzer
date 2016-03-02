fe97de41963d2a40579e491444284c974b82302d
Sascha Bieberstein <sbi0991@gmail.com>
2016-03-02 23:04:28 +0100

    Make updated filed optional on create

--- git/models.py
+++ git/models.py
@@ -5,7 +5,7 @@ class Repository(models.Model):
     url = models.URLField(verbose_name="Repository Clone URL", unique=True)
     name = models.CharField(max_length=120, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(null=True)
+    updated = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
